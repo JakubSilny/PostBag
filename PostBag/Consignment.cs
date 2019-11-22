a381172ee63177fb045ce920e68eee6e4ae62d5f
Jakub Silný <u18.425@pslib.cloud>
2019-11-22 18:54:22 +0100

    2019

---
+++ PostBag/Consignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostBag
+{
+    abstract class Consignment
+    {
+        protected int price;
+        protected string Recipient;
+
+        protected Consignment(global::System.Int32 price, global::System.String recipient)
+        {
+            this.price = price;
+            Recipient = recipient;
+        }
+
+        public abstract ToString()
+        {
+
+        }
+
+
+    }
+}
---
+++ PostBag/Letter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostBag
+{
+    class Letter : Consignment
+    {
+        bool Registered;
+        string Sender;
+
+        public Letter(global::System.Boolean registered, global::System.String sender)
+        {
+            Registered = registered;
+            Sender = sender;
+        }
+
+        public override string Tostring()
+        {
+            string str = "";
+
+            str += "LET> " + "To:" ;
+
+
+            return str;
+        }
+    }
+}
